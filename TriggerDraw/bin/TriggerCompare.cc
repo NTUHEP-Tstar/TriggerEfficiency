8597e53a3a0787b9c72ef8d920ef1f6d69e3b7e8
sam <sam7k9621@gmail.com>
2017-03-05 03:16:52 +0800

    finish trisys first round

--- TriggerDraw/bin/TriggerCompare.cc
+++ TriggerDraw/bin/TriggerCompare.cc
@@ -39,4 +39,5 @@ int main(int argc, char* argv[]){
         PlotCompare(tri);
     }
 
+    PlotSysError("ele27");
 }
--- TriggerDraw/src/TriSysErr.cc
+++ TriggerDraw/src/TriSysErr.cc
@@ -1,22 +1,38 @@
 #include "TriggerEfficiency/TriggerDraw/interface/PlotObj.hpp"
 #include "TFile.h"
 #include "TDirectory.h"
-
+#include "TCanvas.h"
+#include "TGraphErrors.h"
+#include <iostream>
 /*******************************************************************************
 *   Global function
 *******************************************************************************/
+extern void drawtest(){
+    TCanvas* c = new TCanvas();
+    (trinamer.GetDataPt()) -> Draw("AEP");
+    c->SaveAs("test.pdf");
 
+    cout<< (trinamer.GetDataPt()) ->GetY()[3] <<" "<<(trinamer.GetDataPt()) ->GetErrorY(3)<<endl;
+}
 
 extern void PlotSysError(const string& tri){
     InitSysOri(tri);
-//    InitSysNew(tri);
+  
+    drawtest();
+    //    InitSysNew(tri);
 
-    SetGraphName(tri);
+//    SetGraphName(tri);
 
 //    PlotPt(tri);
 //    PlotEta(tri);
 
-    Clean();
+//    Clean();
+}
+
+extern void ConvertTGraph(TGraph* g, TH1* h){
+    double binnum = h->GetXaxis()->GetNbins();
+    cout<<"binnum "<<binnum<<endl;
+    
 }
 
 extern void InitSysOri(const string& tri){
@@ -24,12 +40,13 @@ extern void InitSysOri(const string& tri){
     TFile* file = TFile::Open( ( trinamer.GetSingleData<string>("oripath") ).c_str() );
     
     TH1D* pt;
-    pt = (TH1D*) file->Get( ("pt_scale_"+tri).c_str() );
+    pt = (TH1D*) file->Get( ("pt_"+tri).c_str() );
     TH1D* eta;
-    eta = (TH1D*) file->Get( ("eta_scale_"+tri).c_str() );
+    eta = (TH1D*) file->Get( ("eta_"+tri).c_str() );
     
-    TGraph* _pt  = new TGraph(pt);
-    TGraph* _eta = new TGraph(eta);
+    TGraphErrors* _pt  = new TGraphErrors(pt);
+    TGraphErrors* _eta = new TGraphErrors(eta);
+
 
     trinamer.SetPtData(_pt);
     trinamer.SetEtaData(_eta);
